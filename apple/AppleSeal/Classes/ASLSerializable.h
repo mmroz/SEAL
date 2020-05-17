7400fc270dd1d1445db13c3aa2fd791ae96dee8e
mmroz <markmroz95@gmail.com>
2020-05-17 12:43:23 -0400

    Added coding to serializable

--- apple/AppleSeal/Classes/ASLSerializable.h
+++ apple/AppleSeal/Classes/ASLSerializable.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ASLSerializable<T> : NSObject
+@interface ASLSerializable<T> : NSObject <NSCoding>
     @property (nonatomic, readonly) T object;
 @end
 
--- apple/AppleSeal/Classes/ASLSerializable.mm
+++ apple/AppleSeal/Classes/ASLSerializable.mm
@@ -24,4 +24,14 @@
 //    _serializable = serializable;
 //}
 
+# pragma mark - NSCoding
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+    std::size_t const lengthUpperBound = _serializable->save_size(seal::Serialization::compr_mode_default);
+    NSMutableData * const data = [NSMutableData dataWithLength:lengthUpperBound];
+    std::size_t const actualLength = _serializable->save(static_cast<std::byte *>(data.mutableBytes), lengthUpperBound);
+    [data setLength:actualLength];
+    [coder encodeDataObject:data];
+}
+
 @end
